1df2835566352c630aef4393e2d151498c7e770c
Khalil-Balde1212 <Khalil.Immanuel@gmail.com>
2026-02-02 13:37:45 -0500

    Error Exists Between Keyboard and Computer

--- Lecture 3/aidan.py
+++ Lecture 3/aidan.py
@@ -5,20 +5,34 @@ if __name__ == "__main__":
     kp, ki = 0.00596, 0.028408163
     perror, ierror = 0.0, 0.0
 
-    setpoint = 2000.0
+    setpoint = 1000.0
     dt = 0.02
     initialize_connection()
     send_to_serial(0)
     sleep(1)
 
     while True:
+        try:
+            pass
+        except KeyboardInterrupt:
+            for _ in range(5):
+                send_to_serial(0)
+                sleep(0.1)
+            cleanup()
+            break
+
+        # Simple PI control loop
         current_speed = read_from_serial()  # hypothetical function
         perror = setpoint - current_speed
         ierror += perror * dt
 
+        # Anti-windup for integral error
+        ierror = max(-2.5, min(2.5, ierror))
+        
+
         u = kp * perror + ki * ierror
-        # u *= 255/5
-        u = max(0, min(255, int(u)))  # clamp to 0-255
+        u *= 255/5
+        u = max(0, min(128, int(u)))  # clamp to 0-255
         send_to_serial(u)
 
         sleep(dt)
