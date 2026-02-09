74768f51b477aa503ab0cdbfd9053f20a73fd839
aidanbarber <e.aidanbarber@gmail.com>
2026-02-09 12:46:04 -0500

    simple serial read

---
+++ AidanB.py
@@ -0,0 +1,35 @@
+import serial
+import sys
+import struct
+import matplotlib.pyplot as plt
+
+x = 0
+y = 0
+
+if sys.platform.startswith('win'):
+    ser = serial.Serial('COM3', 9600, timeout=1)
+else:
+    ser = serial.Serial('ttyUSB0', 9600, timeout=1)
+
+plt.ion()
+
+graph = plt.plot(x, y)[0]
+
+print("Starting data collection...")
+ser.write(b'\xff')
+values = []
+
+
+while True:
+    data = ser.read(4)
+    if len(data) == 4:
+        value = struct.unpack('<f', data)[0]
+        y.append(value)
+        x.append(x[-1]+1)
+
+    graph.remove()
+
+    graph = plt.plot(x, y, color='g')[0]
+    plt.xlim(x[0], x[-1])
+
+    plt.pause(0.001)
