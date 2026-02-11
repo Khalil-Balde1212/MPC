754289a46157be7c9a498f5bdd1861c472f6a25c
aidanbarber <e.aidanbarber@gmail.com>
2026-02-11 12:53:05 -0500

    working serial write and read with plot

--- AidanB.py
+++ AidanB.py
@@ -7,6 +7,19 @@ import time
 x = [0]
 y = [0]
 
+
+def on_close(event):
+    value_to_send = 0
+    little_endian_bytes = value_to_send.to_bytes(length=1, byteorder='little')
+
+    ser.write(little_endian_bytes)
+
+    ser.close()
+    plt.close('all')
+    print("Cleanup complete")
+    exit()
+
+
 if sys.platform.startswith('win'):
     ser = serial.Serial('COM3', 9600, timeout=None)
 else:
@@ -14,21 +27,22 @@ else:
 
 plt.ion()
 
+fig, ax = plt.subplots()
+fig.canvas.mpl_connect('close_event', on_close)
+
 time.sleep(2)  # Wait for Arduino reset
 ser.reset_input_buffer()
 
-plt.ion()
-fig, ax = plt.subplots()
 
 try:
     graph = plt.plot(x, y)[0]
     print("Starting data collection...")
-    value_to_send = 255
+    value_to_send = 75
     little_endian_bytes = value_to_send.to_bytes(length=1, byteorder='little')
     ser.write(little_endian_bytes)
 
     while True:
-        data = ser.read(4)  # Just read, don't reset buffer!
+        data = ser.read(4)
 
         if len(data) == 4:
             value = struct.unpack('<f', data)[0]
@@ -36,7 +50,6 @@ try:
             y.append(value)
             x.append(x[-1] + 1)
 
-            # More efficient plotting
             graph.set_data(x, y)
             ax.relim()
             ax.autoscale_view()
@@ -48,10 +61,4 @@ except KeyboardInterrupt:
     print("\nInterrupted! Closing plot...")
 
 finally:
-    value_to_send = 0
-    little_endian_bytes = value_to_send.to_bytes(length=1, byteorder='little')
-    ser.write(little_endian_bytes)
-
-    ser.close()
-    plt.close('all')
-    print("Cleanup complete")
+    on_close(None)
